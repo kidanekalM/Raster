3a78961fcb6d5c075b9d56210a6d46d446f277cf
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-06-25 12:23:42 +0300

    update raster

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-let numRow = 30
-let numCol = 30
-let pixels = new Array(30)
-for(i=0;i<numRow;i++){
-    let row = document.createElement('div');
-    row.className = "row"
-    pixels[i] = new Array(30)
-    for(j=0;j<numCol;j++){
-        let pixel = document.createElement('div')
-        pixel.className = "pixel"
-        row.appendChild(pixel)
-        pixels[i][j] = pixel
+let resolution = 60
+function setup(resolution){
+
+    let numRow = resolution
+    let numCol = resolution
+    let pixels = new Array(resolution)
+    for(i=0;i<numRow;i++){
+        let row = document.createElement('div');
+        row.className = "row"
+        pixels[i] = new Array(30)
+        for(j=0;j<numCol;j++){
+            let pixel = document.createElement('div')
+            pixel.className = "pixel"
+            row.appendChild(pixel)
+            pixels[i][j] = pixel
+        }
+        document.getElementById("wrapper").appendChild(row)
     }
-    document.getElementById("wrapper").appendChild(row)
-}
 // we reverse the array to resemble the cartesian plane
 pixels = pixels.reverse()
 function setPixel(x,y){
@@ -21,20 +24,18 @@ function setPixel(x,y){
 
 document.getElementById("run").addEventListener('click',(e)=>{
     let code = document.getElementById("txtCode").value
-    // console.log(code);
-    // code.forEach(c => {
-    //     c = c.toString()
-    //     console.log(c)
-    //     if((c).includes("setPixel")){
-    //         let y = ((c).substring(c.indexOf('(')+1,c.indexOf(',')))
-    //         let x = ((c).substring(c.indexOf(',')+1,c.indexOf(')')))
-            
-    //         pixels[x][y].className += ' set'
-    //     }
-    //     else{
-    //         console.log(false,"not Found")
-    //     }
-    // })
     eval(code)
+})
 
+}
+setup(60)
+document.getElementById('btn_update').addEventListener('click',function(){
+    let wrapper = document.createElement('div')
+    wrapper.className = 'wrapper'
+    wrapper.id = 'wrapper'
+    wrParent = document.getElementById('wrapper').parentElement
+    wrParent.removeChild(document.getElementById('wrapper'))
+    wrParent.appendChild(wrapper)
+    console.log(document.getElementById('wrapper').children)
+    setup(document.getElementById('txt_res').value)
 })
