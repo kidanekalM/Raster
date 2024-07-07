c428dd9e407ef2c7f2087037622c75792347482e
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-07-07 10:08:26 +0300

    Update codeHighlight.js

--- codeHighlight.js
+++ codeHighlight.js
@@ -10,5 +10,6 @@ dictionary = { "for":"green",
 
 }
 document.getElementById("txtCode").addEventListener('input',function(){
-    document.getElementById("txtCode").value = document.getElementById("txtCode").value.replace("for","<span style='backgroundColor:green'>for</span>")
+    console.log(document.getElementById("txtCode").value)
+    document.getElementById("txtCode").value = document.getElementById("txtCode").value.replace("for","<span style='backgroundColor:"+dictionary['for']+"'>for</span>")
 })
